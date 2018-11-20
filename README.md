09d129911e5d584172a41ddd488b895cd8d055b3
Singular Cabbage <daniellangamma@gmail.com>
2018-11-19 19:43:14 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# halligan_listener
---
+++ listener.py
@@ -0,0 +1,47 @@
+import json
+from json import JSONDecodeError
+from websocket import create_connection
+import requests
+import pyttsx3
+
+# This is a file where I store my login info
+import credentials
+
+engine = pyttsx3.init()
+engine.setProperty('rate', 180)
+
+ws = create_connection("wss://www.halliganhelper.com/ws/ta?subscribe-broadcast")
+
+# replace credentials.login and credentials.password with your own
+r = requests.post('https://www.halliganhelper.com/api/v3/user/login/',
+                  data = {'email': credentials.login, 'password': credentials.password})
+
+def getInfo (res):
+    id = res['data']['id']
+    info = requests.get('https://www.halliganhelper.com/api/v3/school/courses/16/requests/' + str(id), cookies = r.cookies)
+    req = json.loads(info.text)
+
+    out = ("New request: {}, at {}, by {}".format(req['question'], req['where_located'], req['requestor']['first_name']))
+    say (out)
+
+def say (out):
+    engine.say(out)
+    engine.runAndWait()
+
+while (True):
+    ping = {}
+    try:
+        ping = json.loads(ws.recv())
+    except JSONDecodeError:
+        pass
+    else:
+        if (ping['type'] == 'request_created'):
+            getInfo(ping)
+        elif (ping['type'] == 'request_updated'):
+            # checked out or edited request
+            pass
+        elif (ping['type'] == 'request_removed'):
+            # resolved or canceled
+            pass
+
+ws.close()
